f161f418111500efe3556a17182451acedf5e223
Aditi Upadhayay <upadhyayaditi94@gmail.com>
2025-04-18 16:15:10 +0530

    Auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -35,3 +35,20 @@ export const registerUser = async (req, res) => {
       res.status(500).json({ message: "Server error" });
     }
   };
+
+  // get current user
+
+export const getCurrentUser = async (req, res) => {
+    try {
+      // req.user is set by the protect middleware
+      // We don't need to find the user again since the middleware already did that
+      if (!req.user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      
+      res.status(200).json(req.user);
+    } catch (error) {
+      console.error("Get current user error:", error);
+      res.status(500).json({ message: "Server error" });
+    }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authControllers.js";
+import {getCurrentUser, registerUser, loginUser } from "../controllers/authControllers.js";
+import protect from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// protected
+router.get('/me', protect, getCurrentUser);
 
 export default router;
